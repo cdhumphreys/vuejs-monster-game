0b87a77264b920b9e0bdc32af137f511ccdf8e43
Carl Humphreys <humphreys.carl.d@gmail.com>
2020-01-28 18:04:06 +0000

    player and monster actions and game states

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-const gameStates = {
-    menu: 'MENU',
-    inProgress: 'IN_PROGRESS',
-    won: 'WON',
-    lost: 'LOST'
-};
+const GAME_STATE_MENU = 'MENU';
+const GAME_STATE_IN_PROGRESS = 'IN_PROGRESS';
+const GAME_STATE_WON = 'WON';
+const GAME_STATE_LOST = 'LOST';
 
 new Vue({
     el: '#app',
     data: {
-        gameState: gameStates.menu,
+        gameState: GAME_STATE_MENU,
         monsterHealth: 100,
-        playerHealth: 100
+        playerHealth: 100,
+        playerMaxAttackDamage: 20,
+        playerSpecialAttackModifier: 1.2,
+        playerMaxHealAmount: 20,
+        monsterMaxAttackDamage: 15,
+        battleLog: [],
+        gameStates: {
+            MENU: GAME_STATE_MENU,
+            IN_PROGRESS: GAME_STATE_IN_PROGRESS,
+            WON: GAME_STATE_WON,
+            LOST: GAME_STATE_LOST
+        }
     },
     computed: {
-        showStart: function() {
-            return (
-                this.gameState == gameStates.menu ||
-                this.gameState == gameStates.won ||
-                this.gameState == gameStates.lost
-            );
-        },
         playerClasses: function() {
             return {
                 'healthbar': true,
@@ -39,7 +41,52 @@ new Vue({
     },
     methods: {
         startGame: function() {
-            
+            this.battleLog = [];
+            this.gameState = this.gameStates.IN_PROGRESS;
+        },
+
+        addBattleLog: function(agent, target, damage) {
+            this.battleLog.push({
+                agent: agent,
+                target: target,
+                damage: damage
+            });
+        },
+
+        monsterAttack: function() {
+            const damage = -1 * Math.floor(Math.random() * this.monsterMaxAttackDamage) + 1;
+            this.playerHealth += damage;
+            this.addBattleLog(agents.MONSTER, agents.PLAYER, damage);
+        },
+
+        playerAttack: function() {
+            const damage = -1 * Math.floor(Math.random() * this.playerMaxAttackDamage) + 1;
+            this.monsterHealth += damage;
+            this.addBattleLog(agents.PLAYER, agents.MONSTER, damage);
+        },
+
+        playerSpecialAttack: function() {
+            const damage = -1 * Math.Floor((Math.floor(Math.random() * this.playerMaxAttackDamage) + 1) * this.playerSpecialAttackModifier);
+            this.monsterHealth += damage;
+            this.addBattleLog(agents.PLAYER, agents.MONSTER, damage);
+        },
+
+        playerHeal: function() {
+            const heal = Math.floor(Math.random() * this.playerMaxHealAmount) + 1;
+            this.playerHealth += heal;
+            this.addBattleLog(agents.PLAYER, agents.PLAYER, heal);
+        }
+    },
+    watch: {
+        monsterHealth: function(monsterHP) {
+            if (monsterHP === 0) {
+                this.gameState = this.gameStates.WON;
+            }
+        },
+        playerHealth: function(playerHP) {
+            if (playerHP ===0) {
+                this.gameState = this.gameStates.LOST;
+            }
         }
     }
 });
