6f130cc75e8db8d5db0b29ead3828ef8d0afc46e
Carl Humphreys <humphreys.carl.d@gmail.com>
2020-01-28 16:14:52 +0000

    health bars styling + functionality

--- app.js
+++ app.js
@@ -5,6 +5,29 @@ new Vue({
         playerHealth: 100
     },
     computed: {
+        showStart: function() {
+            return (
+                this.gameState == gameStates.menu ||
+                this.gameState == gameStates.won ||
+                this.gameState == gameStates.lost
+            );
+        },
+        playerClasses: function() {
+            return {
+                'healthbar': true,
+                'healthbar--green': this.playerHealth >= 75,
+                'healthbar--orange': this.playerHealth < 75 && this.playerHealth > 40,
+                'healthbar--red': this.playerHealth <= 40,
+            }
+        },
+        monsterClasses: function() {
+            return {
+                'healthbar': true,
+                'healthbar--green': this.monsterHealth >= 75,
+                'healthbar--orange': this.monsterHealth < 75 && this.monsterHealth > 40,
+                'healthbar--red': this.monsterHealth <= 40,
+            }
+        }
     },
     methods: {
         startGame: function() {
